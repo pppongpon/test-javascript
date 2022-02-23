a95bdec96344007c93029c94ce05895a7965fae5
pppongpon <85613209+pppongpon@users.noreply.github.com>
2022-02-24 01:55:53 +0700

    Create VenueBookingSystem.js

---
+++ VenueBookingSystem.js
@@ -0,0 +1,319 @@
+const room = [
+  {
+    "id": 1,
+    "roomId": "A101",
+    "startTime": "2019-09-28 13:00:00",
+    "endTime": "2019-09-28 14:00:00",
+    "title": "Lunch with Petr"
+  },
+  {
+    "id": 2,
+    "roomId": "A101",
+    "startTime": "2019-09-28 14:00:00",
+    "endTime": "2019-09-28 15:00:00",
+    "title": "Sales Weekly Meeting"
+  },
+  {
+    "id": 3,
+    "roomId": "A101",
+    "startTime": "2019-09-28 16:00:00",
+    "endTime": "2019-09-28 18:00:00",
+    "title": "Anastasia Website Warroom"
+  },
+  {
+    "id": 4,
+    "roomId": "A101",
+    "startTime": "2019-09-29 13:00:00",
+    "endTime": "2019-09-29 14:00:00",
+    "title": "One-on-One Session"
+  },
+  {
+    "id": 5,
+    "roomId": "A101",
+    "startTime": "2019-09-29 16:00:00",
+    "endTime": "2019-09-29 18:00:00",
+    "title": "UGC Sprint Planning"
+  },
+  {
+    "id": 6,
+    "roomId": "A102",
+    "startTime": "2019-09-30 09:00:00",
+    "endTime": "2019-10-04 18:00:00",
+    "title": "5-Day Design Sprint Workshop"
+  },
+  {
+    "id": 7,
+    "roomId": "Auditorium",
+    "startTime": "2019-09-19 09:00:00",
+    "endTime": "2019-09-23 19:00:00",
+    "title": "Thai Tech Innovation 2019"
+  },
+  {
+    "id": 8,
+    "roomId": "A101",
+    "startTime": "2019-09-28 10:00:00",
+    "endTime": "2019-09-28 13:00:00",
+    "title": "Raimonland project"
+  },{
+    "id": 9,
+    "roomId": "A102",
+    "startTime": "2019-09-30 18:00:00",
+    "endTime": "2019-09-30 20:00:00",
+    "title": "Management Meetinng"
+  },{
+    "id": 10,
+    "roomId": "A101",
+    "startTime": "2019-10-04 14:00:00",
+    "endTime": "2019-10-06 11:00:00",
+    "title": "3-day workshop Corgi costume"
+  },{
+    "id": 11,
+    "roomId": "A101",
+    "startTime": "2022-02-22 14:00:00",
+    "endTime": "2022-02-22 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 12,
+    "roomId": "A101",
+    "startTime": "2022-02-23 14:00:00",
+    "endTime": "2022-02-23 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 13,
+    "roomId": "A101",
+    "startTime": "2022-02-24 14:00:00",
+    "endTime": "2022-02-24 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 14,
+    "roomId": "A101",
+    "startTime": "2022-02-25 14:00:00",
+    "endTime": "2022-02-25 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 15,
+    "roomId": "A101",
+    "startTime": "2022-02-26 14:00:00",
+    "endTime": "2022-02-26 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 16,
+    "roomId": "A101",
+    "startTime": "2022-02-27 14:00:00",
+    "endTime": "2022-02-27 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 17,
+    "roomId": "A101",
+    "startTime": "2022-02-28 14:00:00",
+    "endTime": "2022-02-28 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 18,
+    "roomId": "A101",
+    "startTime": "2022-02-28 15:00:00",
+    "endTime": "2022-02-28 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 19,
+    "roomId": "A101",
+    "startTime": "2022-03-01 14:00:00",
+    "endTime": "2022-03-03 15:00:00",
+    "title": "test-book"
+  },{
+    "id": 20,
+    "roomId": "A101",
+    "startTime": "2022-02-22 15:00:00",
+    "endTime": "2022-02-23 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 21,
+    "roomId": "A101",
+    "startTime": "2022-02-20 15:00:00",
+    "endTime": "2022-02-20 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 22,
+    "roomId": "A101",
+    "startTime": "2022-02-21 15:00:00",
+    "endTime": "2022-02-21 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 23,
+    "roomId": "A101",
+    "startTime": "2022-02-19 15:00:00",
+    "endTime": "2022-02-20 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 24,
+    "roomId": "A101",
+    "startTime": "2022-02-28 15:00:00",
+    "endTime": "2022-02-28 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 25,
+    "roomId": "A101",
+    "startTime": "2022-03-1 15:00:00",
+    "endTime": "2022-03-2 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 26,
+    "roomId": "A101",
+    "startTime": "2022-03-2 17:00:00",
+    "endTime": "2022-03-5 16:00:00",
+    "title": "test-book"
+  },{
+    "id": 26,
+    "roomId": "A101",
+    "startTime": "2022-03-5 17:00:00",
+    "endTime": "2022-03-6 16:00:00",
+    "title": "test-book"
+  },
+  {
+    "id": 26,
+    "roomId": "A101",
+    "startTime": "2022-03-6 17:00:00",
+    "endTime": "2022-03-7 16:00:00",
+    "title": "test-book"
+  },
+]
+
+const checkAvailability = (roomId, startTime, endTime) => {
+  let bookstart = Date.parse(startTime)/1000
+  let bookend = Date.parse(endTime)/1000
+  let roomStatus = true
+
+  room.map((subRoom) => {
+    console.log('==== ',subRoom.roomId,' ====')
+    let roomstart = Date.parse(subRoom.startTime)/1000
+    let roomend = Date.parse(subRoom.endTime)/1000
+    if(subRoom.roomId == roomId){
+      if(bookstart >= roomend){
+        //console.log("room start ",subRoom.startTime, " to ", subRoom.endTime)
+        //console.log("your book is " ,startTime ," to ", endTime,"can book")
+      }else if(bookstart < roomend && bookend <= roomstart){
+        //console.log("room start ",subRoom.startTime, " to ", subRoom.endTime)
+        //console.log("your book is " ,startTime ," to ", endTime,"can book")
+      }else{
+        //console.log("can't book")
+        //console.log("your book is ",startTime , " to ",endTime )
+        //console.log(subRoom.roomId,"room time is ",subRoom.startTime , " to ",subRoom.endTime )
+        roomStatus = false
+      }
+    }
+  })
+  return roomStatus
+}
+
+const getBookingsForWeek = (roomId, weekNo) => {
+  let myBook = []
+  room.map((subRoom) => {
+    if(subRoom.roomId == roomId){
+      let [startDate, startTime] = subRoom.startTime.split(" ")
+      let [endDate, endTime] = subRoom.endTime.split(" ")
+      //console.log("room start date ",Number(startYear), Number(startMonth), Number(startDay), "---", "room end date ", Number(endYear), Number(endMonth), Number(endDay))
+      
+      // ============================================== set to day date ===================================================
+      let myDate = new Date();
+      let today = myDate.getDay(); // check today what day of this week
+      let [todayYear, todayMonth, todayDay] = [
+        new Date(myDate).getFullYear(), 
+        new Date(myDate).getMonth(), 
+        new Date(myDate).getDate()
+      ]; // separate today date to year-month-day
+      //console.log("today = ",todayYear, todayMonth+1, todayDay)
+      // ============================================== find first and last day of week ===================================
+      let fromDate = new Date();
+      let toDate = new Date();
+      let dateBefore = (24*60*60*1000) * today; //find how many days before today
+      let dateAfter = (24*60*60*1000) * (6-today); //find how many days after today
+      fromDate.setTime(fromDate.getTime() - dateBefore); //got first day of week
+      toDate.setTime(toDate.getTime() + dateAfter); //got last day of week
+      // ============================================== find first and last day of next week ===================================
+      let fromNextWeekDate = new Date();
+      let toNextWeekDate = new Date();
+      let firstDayOfNextWeek = (24*60*60*1000) * (7-today); //find first day of next week
+      let lastDayOfNextWeek = (24*60*60*1000) * (13-today); //find last day of next week
+      fromNextWeekDate.setTime(fromNextWeekDate.getTime() + firstDayOfNextWeek); 
+      toNextWeekDate.setTime(toNextWeekDate.getTime() + lastDayOfNextWeek); 
+
+      //console.log(subRoom.id, fromNextWeekDate," | ", toNextWeekDate)
+      // ============================================== set year-month-day ================================================
+      let [bookStartYear, bookStartMonth, bookStartDate] = [
+        new Date(subRoom.startTime).getFullYear(), 
+        new Date(subRoom.startTime).getMonth(), 
+        new Date(subRoom.startTime).getDate()
+      ];// separate book startTime 
+      let [yearFrom, monthFrom, dayFrom] = [
+        new Date(fromDate).getFullYear(), 
+        new Date(fromDate).getMonth(), 
+        new Date(fromDate).getDate()
+      ];// separate first day of this 
+      let [yearTo, monthTo, dayTo] = [
+        new Date(toDate).getFullYear(), 
+        new Date(toDate).getMonth(), 
+        new Date(toDate).getDate()
+      ];//separate last day of this week 
+      let [nextWeekYear, nextWeekMonth, nextWeekDay] = [
+        new Date(fromNextWeekDate).getFullYear(), 
+        new Date(fromNextWeekDate).getMonth(), 
+        new Date(fromNextWeekDate).getDate()
+      ];//separate first day of next week 
+      let [lastNextWeekYear, lastNextWeekMonth, lastNextWeekDay] = [
+        new Date(toNextWeekDate).getFullYear(), 
+        new Date(toNextWeekDate).getMonth(), 
+        new Date(toNextWeekDate).getDate()
+      ];//separate last day of next week 
+
+      if(weekNo == 0){
+        if(bookStartYear == todayYear && bookStartMonth == todayMonth && bookStartDate == todayDay){
+          //console.log("room today = ", subRoom)
+          myBook.push(subRoom)
+        }
+      }else if(weekNo == 1){
+        //console.log("book : ",bookStartYear, bookStartMonth+1, bookStartDate)
+        //console.log("from : ",yearFrom, monthFrom+1, dayFrom)
+        //console.log("to : ",yearTo, monthTo+1, dayTo)
+
+        if(yearFrom == bookStartYear && yearTo >= bookStartYear && monthFrom == bookStartMonth || monthTo >= bookStartMonth){ //bookYear == todayYear and bookMonth == todayMonth
+          if(bookStartDate >= dayFrom && bookStartDate <= dayTo){
+            //console.log(subRoom)
+            myBook.push(subRoom)
+          }
+        }
+      }else if(weekNo == 2){
+        if(nextWeekYear == bookStartYear && lastNextWeekYear >= bookStartYear && nextWeekMonth == bookStartMonth || lastNextWeekMonth >= bookStartMonth){
+          //console.log("bookStartMonth = ", bookStartMonth, "nextWeekMonth = ", nextWeekMonth, "lastNextWeekMonth = ", lastNextWeekMonth)
+          if(nextWeekMonth == bookStartMonth){
+            //console.log("bookStartDate = ", bookStartDate)
+            //console.log("nextWeekDay = ", nextWeekDay)
+            //console.log("lastNextWeekDay = ", lastNextWeekDay)
+            if(bookStartDate >= nextWeekDay){
+              //console.log(subRoom)
+              myBook.push(subRoom)
+            }
+          }else if(lastNextWeekMonth == bookStartMonth){
+            //console.log("bookStartDate = ", bookStartDate)
+            //console.log("nextWeekDay = ", nextWeekDay)
+            //console.log("lastNextWeekDay = ", lastNextWeekDay)
+
+            if(lastNextWeekDay >= bookStartDate){
+              //console.log(subRoom)
+              myBook.push(subRoom)
+            }
+          }
+        }
+      }
+    }
+  })
+  return(myBook)
+} 
+
+let check = checkAvailability("A101", "2022-02-22 10:00:00", "2022-02-22 12:00:00")
+console.log(check)
+
+// 0 = today
+// 1 this week
+// 2 next week
+let returnBook = getBookingsForWeek("A101",1)
+console.log("book = ",returnBook)
